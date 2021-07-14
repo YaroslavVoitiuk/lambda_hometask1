67eb14c22825a41b5c9bde1a19682a7fa827ba65
Yaroslav Voitiuk <yaroslav.voitiuk@gmail.com>
2021-07-14 08:43:54 +0300

    1 task fixed

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
     BinaryOperator<Integer> sub = (x,y) -> x - y;
     BinaryOperator<Integer> mul = (x,y) -> x * y;
     BinaryOperator<Integer> dev = (x,y) -> {
-        if (y == 0) return 0;
+        if (y == 0) throw new ArithmeticException("На ноль делить нельзя!");
         return x / y;
     };
 
@@ -16,5 +16,7 @@ public class Calculator {
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     Predicate<Integer> isPositive = x -> x > 0;
     Consumer<Integer> result = System.out::println;
+
+
 
 }
